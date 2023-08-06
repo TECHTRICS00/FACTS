e93f28c8777f4858cc86d6c2ea487293f48df52c
TECHTRICS00 <141350219+TECHTRICS00@users.noreply.github.com>
2023-08-06 20:39:17 +0530

    Add files via upload

---
+++ feed.js
@@ -0,0 +1,8 @@
+
+$action = $_GET["action"];
+$myText = $_POST["mytext"];
+
+if($action = "save") {
+  $targetFolder = "/path/to/folder";
+  file_put_contents($targetFolder."mytext.txt", $myText);
+}
